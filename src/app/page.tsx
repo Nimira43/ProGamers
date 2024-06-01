c19f0ab8ad199dcbab811d704d8d77afe76cf3b5
Nick Rabone <n.rabone72@aol.com>
2024-06-01 19:43:05 +0100

    Updating changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@nextui-org/react";
+import { FaRegFaceDizzy } from "react-icons/fa6";
+
 export default function Home() {
   return (
    <div>
-    Hello
+    <h1 className="text-3xl bg-[#111] text-[#ff4500] font-bold p-5 mb-3">NimiraTech &copy; 2024</h1>
+    <Button color='primary' variant='bordered' startContent={<FaRegFaceDizzy size={20} />}>
+      Launch
+    </Button>
    </div> 
   );
 }
