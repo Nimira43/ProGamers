990e3ece8d9ce0fd1f4e19dfbb3cb7c8e9a4abbf
Nick Rabone <n.rabone72@aol.com>
2024-06-01 20:12:36 +0100

    Gcreated and routed Members Page.

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function Members() {
+export default function MembersPage() {
     return (
-        <div>Members</div>
+        <div>
+            <h3 className="text-3xl">Placeholder for Members Page </h3>
+            <Link href='/'>Return to Home Page</Link>  
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { Button } from "@nextui-org/react";
 import { FaRegFaceDizzy } from "react-icons/fa6";
+import Link from 'next/link';
 
 export default function Home() {
   return (
    <div>
     <h1 className="text-3xl bg-[#111] text-[#ff4500] font-bold p-5 mb-3">NimiraTech &copy; 2024</h1>
-    <Button color='primary' variant='bordered' startContent={<FaRegFaceDizzy size={20} />}>
+    <Button 
+      as={Link}
+      href='/members'
+      color='primary' 
+      variant='bordered' 
+      startContent={<FaRegFaceDizzy size={20} />}
+
+    >
       Launch
     </Button>
    </div> 
