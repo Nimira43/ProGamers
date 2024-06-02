bf99961feb103e51e5b859f9ab1b9bb0ac3a3cbe
Nick Rabone <n.rabone72@aol.com>
2024-06-02 02:56:06 +0100

    Setting up Register page

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import { LoginSchema, loginSchema } from '@/lib/schemas/loginSchema'
+import { RegisterSchema, registerSchema } from '@/lib/schemas/registerSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { GiPadlock } from 'react-icons/gi'
 
-export default function LoginForm() {
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm<LoginSchema>({
-        resolver: zodResolver(loginSchema),
+export default function RegisterForm() {
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
         mode: 'onTouched'
     })
 
-    const onSubmit = (data: LoginSchema) => {
+    const onSubmit = (data: RegisterSchema) => {
         console.log(data)
     }
 
@@ -23,9 +23,9 @@ export default function LoginForm() {
                 <div className='flex flex-col gap-2 items-center text-[#111]'>
                     <div className='flex flex-row items-center gap-3'>
                         <GiPadlock size={30} className='text-[#ff4500]'/>
-                        <h1 className='text-3xl font-semibold'>Login</h1>
+                        <h1 className='text-3xl font-semibold'>Register</h1>
                     </div>
-                    <p className='text-[#333]'>Please enter login details</p>
+                    <p className='text-[#333]'>Please complete registration details</p>
                 </div>
             </CardHeader>
             <CardBody>
