7f4324e3454f282bbc16560a974d7121cdae4878
Nick Rabone <n.rabone72@aol.com>
2024-06-02 04:14:02 +0100

    Working on NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
  
-export const { 
-    handlers: { GET, POST }, 
-    auth,
-} = NextAuth({
-  providers: [GitHub],
+const prisma = new PrismaClient()
+ 
+export const { handlers: { GET, POST }, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt'},
+  ...authConfig,
 })
