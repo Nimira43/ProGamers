c076325e91acfc8e05dc82b500639697e341617f
Nick Rabone <n.rabone72@aol.com>
2024-06-02 02:42:03 +0100

    Login form mostly completed with full validation using Zod

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,15 +1,22 @@
 'use client'
 
+import { LoginSchema, loginSchema } from '@/lib/schemas/loginSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { GiPadlock } from 'react-icons/gi'
 
 export default function LoginForm() {
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm()
-    const onSubmit = (data: any) => {
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm<LoginSchema>({
+        resolver: zodResolver(loginSchema),
+        mode: 'onTouched'
+    })
+
+    const onSubmit = (data: LoginSchema) => {
         console.log(data)
     }
+
     return (
         <Card className='w-2/5 mx-auto'>
             <CardHeader className='flex flex-col items-center justify-center'>
@@ -28,7 +35,7 @@ export default function LoginForm() {
                             defaultValue=''
                             label='Email'
                             variant='bordered'
-                            {...register('email', {required: 'Email is required'})}
+                            {...register('email')}
                             isInvalid={!!errors.email}
                             errorMessage={errors.email?.message as string}
                         />
@@ -37,7 +44,7 @@ export default function LoginForm() {
                             label='Password'
                             variant='bordered'
                             type='password'
-                            {...register('password', {required: 'Password is required'})}
+                            {...register('password')}
                             isInvalid={!!errors.password}
                             errorMessage={errors.password?.message as string}
                         />
