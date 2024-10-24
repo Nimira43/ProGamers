8e24cbc0f6972cba86565c53b979719990b75e6b
Nimira43 <n.rabone72@aol.com>
2024-10-24 01:33:07 +0100

    Tailwind working fine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <div className='text-3xl p-5 bg-[#111] text-[#ff4500]'>
       <h1>Soulz</h1>
     </div>
   )
