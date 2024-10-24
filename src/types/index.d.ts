c0157e5514a944ac994c3e2b101862c972ec274d
Nimira43 <n.rabone72@aol.com>
2024-10-24 01:11:12 +0100

    Project rebooted

--- src/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ZodIssue } from 'zod'
-
-type ActionResult<T> = { status: 'success', data: T } |
-                       { status: 'error', error: string | ZodIssue[] }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -6,12 +5,15 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
 export default config;
