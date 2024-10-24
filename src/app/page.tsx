7d45b0051b9ed52a786aa1645f911421a03a8e4d
Nimira43 <n.rabone72@aol.com>
2024-10-24 02:45:38 +0100

    Icons and NExt UI components working fine.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@nextui-org/react'
 import React from 'react'
+import { FaRegSmileBeam } from 'react-icons/fa'
 
 export default function page() {
   return (
     <div>
       <h1>Soulz</h1>
+      <Button color='primary' variant='bordered' startContent={<FaRegSmileBeam size={20}/>}>
+        Submit
+      </Button>
     </div>
   )
 }
