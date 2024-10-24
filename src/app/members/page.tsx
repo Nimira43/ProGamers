6cd2d7f5f9ccc71cbbf049804225dd39568dea20
Nimira43 <n.rabone72@aol.com>
2024-10-24 02:58:24 +0100

    Basic routing to and from home page to members page

---
+++ src/app/members/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h3 className='text-3xl'>Members Page</h3>
+      <Link href='/'>
+        Home
+      </Link>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { Button } from '@nextui-org/react'
 import React from 'react'
 import { FaRegSmileBeam } from 'react-icons/fa'
+import Link from 'next/link'
 
 export default function page() {
   return (
     <div>
       <h1>Soulz</h1>
-      <Button color='primary' variant='bordered' startContent={<FaRegSmileBeam size={20}/>}>
+      <Button
+        as={Link}
+        href='/members' 
+        color='primary' 
+        variant='bordered' 
+        startContent={<FaRegSmileBeam size={20} />}
+      >
         Submit
       </Button>
     </div>
