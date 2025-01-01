320486004a375c8393b3ec9ba3a8a1465313e798
Nimira43 <n.rabone72@aol.com>
2025-01-01 04:53:02 +0000

    Basic  CardBody styled for LoginForm

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,6 +1,5 @@
-import { Card, CardHeader } from '@nextui-org/react'
+import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
 import { RiLockLine } from 'react-icons/ri'
-
 
 export default function LoginForm() {
   return (
@@ -9,11 +8,34 @@ export default function LoginForm() {
         <div className='flex flex-col gap-2 items-center'>
           <div className='flex flex-row items-center gap-3'>
             <RiLockLine size={30} className='text-[#ff4500]'/>
-            <h1 className='text-3xl font-semibold'>Login</h1>
+            <h1 className='text-3xl font-semibold uppercase'>Login</h1>
           </div>
-          <p className='text-[#111]'>Welcome back to ProGamers</p>         
+          <p className='text-[#111] uppercase'>Welcome back to ProGamers</p>         
         </div>
       </CardHeader>
+      <CardBody>
+        <form action=''>
+          <div className='space-y-4'>
+            <Input 
+              label='Email'
+              variant='bordered'
+            />
+            <Input 
+              label='Password'
+              variant='bordered'
+              type='password'
+            />
+            <Button 
+              fullWidth
+              // color='warning' 
+              type='submit'
+              className='bg-[#111] text-opacity-100 text-[#fffaf0] font-semibold uppercase tracking-wide'
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </CardBody>
     </Card>
   )
 }
