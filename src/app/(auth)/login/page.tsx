d491088f06aaddc624c23bc305d2682dbfb6bb63
Nimira43 <n.rabone72@aol.com>
2025-01-01 05:30:43 +0000

    Removed inline style from LoginPage and brought that style in from Globals

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,11 +3,7 @@ import LoginForm from './LoginForm'
 export default function LoginPage() {
   return (
     <div 
-      className='flex items-center justify-center'
-      style={{
-        height: 'calc(100vh - 64px)'
-      }}
-      >
+      className='flex items-center justify-center vertical-center'>
       <LoginForm />
     </div>
   )
