d6218d58121870f58bd7d9398104697b68f12e72
Nimira43 <n.rabone72@aol.com>
2025-01-01 04:38:24 +0000

    Designed CardHeader for LoginForm

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,5 +1,19 @@
+import { Card, CardHeader } from '@nextui-org/react'
+import { RiLockLine } from 'react-icons/ri'
+
+
 export default function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <Card className='w-2/5 mx-auto'>
+      <CardHeader className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col gap-2 items-center'>
+          <div className='flex flex-row items-center gap-3'>
+            <RiLockLine size={30} className='text-[#ff4500]'/>
+            <h1 className='text-3xl font-semibold'>Login</h1>
+          </div>
+          <p className='text-[#111]'>Welcome back to ProGamers</p>         
+        </div>
+      </CardHeader>
+    </Card>
   )
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import Link from 'next/link'
+import LoginForm from './LoginForm'
 
 export default function LoginPage() {
   return (
     <div>
-      <h3 className='text-3xl'>Login Page</h3>
-      <Link href='/'>
-        Home
-      </Link>
+      <LoginForm />
     </div>
   )
 }
