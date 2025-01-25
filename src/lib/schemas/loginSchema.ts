467d3a97409cbf5ea482d360b82ae0682b9ece12
Nimira43 <n.rabone72@aol.com>
2025-01-25 21:04:56 +0000

    Customised the password field in the schema to have a better validation error message.

--- src/lib/schemas/loginSchema.ts
+++ src/lib/schemas/loginSchema.ts
@@ -2,7 +2,9 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8)
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.'
+  })
 })
 
 export type LoginSchema = z.infer<typeof loginSchema>
