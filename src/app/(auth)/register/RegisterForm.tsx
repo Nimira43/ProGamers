77b9930b9e347e613b15f626bd8ca84079e58b92
Nimira43 <n.rabone72@aol.com>
2025-02-19 23:04:44 +0000

    Updated imports and const variables for RegisterForm

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,63 +1,61 @@
-// 'use client'
+import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Card, CardBody, CardHeader} from '@nextui-org/react'
+import { useForm } from 'react-hook-form'
+import { RiLockLine } from 'react-icons/ri'
 
-// import { loginSchema, LoginSchema } from '@/lib/schemas/loginSchema'
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
-// import { useForm } from 'react-hook-form'
-// import { RiLockLine } from 'react-icons/ri'
-l;////////////////////////////////////+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// export default function LoginForm() {
-//   const { register, handleSubmit, formState: {errors, isValid} } = useForm<LoginSchema>({
-//     resolver: zodResolver(loginSchema),
-//     mode: 'onTouched'
-//   })
-//   const onSubmit = (data: LoginSchema) => {
-/************************************************************** R544444444444444444444444444444TTTTTTTAqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq-0/     console.log(data)
-//   }
+export default function RegisterForm() {
+  const { register, handleSubmit, formState: {errors, isValid} } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: 'onTouched'
+  })
 
-//   return (
-//     <Card className='w-2/5 mx-auto font'>
-//       <CardHeader className='flex flex-col items-center justify-center'>
-//         <div className='flex flex-col gap-2 items-center'>
-//           <div className='flex flex-row items-center gap-3'>
-//             <RiLockLine size={30} className='text-[#ff4500]'/>
-//             <h1 className='text-3xl font-semibold uppercase'>Login</h1>
-//           </div>
-//           <p className='text-[#111] text-center'>Welcome back to ProGamers</p>         
-//         </div>
-//       </CardHeader>
-//       <CardBody>
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//           <div className='space-y-4'>
-//             <Input 
-//               defaultValue=''
-//               label='Email'
-//               variant='bordered'
-//               {...register('email')}
-//               isInvalid={!!errors.email}
-//               errorMessage={errors.email?.message as string}
-//             />
-//             <Input 
-//               defaultValue=''
-//               label='Password'
-//               variant='bordered'
-//               type='password'
-//               {...register('password')}
-//               isInvalid={!!errors.password}
-//               errorMessage={errors.password?.message as string}
-//             />
-//             <Button 
-//               isDisabled={!isValid}
-//               fullWidth
-//               // color='warning' 
-//               type='submit'
-//               className='bg-[#111] text-opacity-100 text-[#fffaf0] font-semibold uppercase tracking-wide'
-//             >
-//               Submit
-//             </Button>
-//           </div>
-//         </form>
-//       </CardBody>
-//     </Card>
-//   )
-// }
+  const onSubmit = (data: RegisterSchema) => {
+    console.log(data)
+  }
+
+  return (
+    <Card className='w-2/5 mx-auto font'>
+      <CardHeader className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col gap-2 items-center'>
+          <div className='flex flex-row items-center gap-3'>
+            <RiLockLine size={30} className='text-[#ff4500]'/>
+            <h1 className='text-3xl font-semibold uppercase'>Login</h1>
+          </div>
+          <p className='text-[#111] text-center'>Welcome back to ProGamers</p>         
+        </div>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='space-y-4'>
+            <Input 
+              defaultValue=''
+              label='Email'
+              variant='bordered'
+              {...register('email')}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message as string}
+            />
+            <Input 
+              defaultValue=''
+              label='Password'
+              variant='bordered'
+              type='password'
+              {...register('password')}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
+            />
+            <Button 
+              isDisabled={!isValid}
+              fullWidth
+              type='submit'
+              className='bg-[#111] text-opacity-100 text-[#fffaf0] font-semibold uppercase tracking-wide'
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,15 @@ const config: Config = {
   theme: {
     extend: {
       color: {
-        'prime': '#ff4500',
+        'feature': '#ff4500',
         'support': '#87ceeb',
         'dark': '#111',
         'light': '#fffcfa',
         'grey-dark': '#333',
         'grey-medium': '#999',
         'grey-light': '#ccc',
-        'grey-light-extra': '#eee'
-      }
+        'grey-light-extra': '#eee',
+      },
     },
   },
   darkMode: 'class',
