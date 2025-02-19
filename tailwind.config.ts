76e645b1bf405789d8569f5b7dab73e80cafa298
Nimira43 <n.rabone72@aol.com>
2025-02-19 22:07:24 +0000

    Set up theme colours.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      color: {
+        'prime': '#ff4500',
+        'support': '#87ceeb',
+        'dark': '#111',
+        'light': '#fffcfa',
+        'grey-dark': '#333',
+        'grey-medium': '#999',
+        'grey-light': '#ccc',
+        'grey-light-extra': '#eee'
+      }
     },
   },
   darkMode: 'class',
