442583c1c189b2b0817b09ee64774a4f687d9535
Nimira43 <n.rabone72@aol.com>
2025-04-21 23:43:40 +0100

    Ensure the errors returned are displayed on the frontend, providing clear feedback to the user

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -3,5 +3,9 @@
 import { RegisterSchema } from '@/lib/schemas/registerSchema'
 
 export async function registerUser(data: RegisterSchema) {
+  const validated = registerSchema.safeParse(data)
 
+  if (!validated.success) {
+    return {error: validated.error.errors}
+  }
 }
