e4e1f171475092ae8983d35a3704848c2e1aa6c4
Nimira43 <n.rabone72@aol.com>
2025-04-22 05:29:59 +0100

    Checking for existing user using email field.

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,5 +1,6 @@
 'user server'
 
+import { prisma } from '@/lib/prisma'
 import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema'
 import bcrypt from 'bcryptjs'
 
@@ -7,9 +8,13 @@ export async function registerUser(data: RegisterSchema) {
   const validated = registerSchema.safeParse(data)
 
   if (!validated.success) {
-    return {error: validated.error.errors}
+    return { error: validated.error.errors }
   }
 
   const { name, email, password } = validated.data
   const hashedPassword = await bcrypt.hash(password, 10)
+  
+  const existingUser = await prisma.user.findUnique({
+    where: { email }
+  })
 }
