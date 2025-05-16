5bf3494dbb7ff845c6ca327f9da15c8fa4fa8aad
Nimira43 <n.rabone72@aol.com>
2025-05-16 22:39:48 +0100

    Setting up providers

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,14 @@
 import Credentials from 'next-auth/providers/credentials'
 
 import type { NextAuthConfig } from 'next-auth'
+import { loginSchema } from './lib/schemas/loginSchema'
  
 export default {
-  providers: [Credentials],
+  providers: [Credentials({
+    name: 'credentials',
+    async authorize(creds) {
+      const validated = loginSchema.safeParse(creds)
+    
+    }
+  })],
 } satisfies NextAuthConfig
