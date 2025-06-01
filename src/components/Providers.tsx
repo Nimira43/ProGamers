f66cbed7c752900bb4c43a45ea5d57aa1091bc80
Nimira43 <n.rabone72@aol.com>
2025-06-01 05:15:08 +0100

    Imported ReactToastify.css

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,6 +3,7 @@
 import { NextUIProvider } from '@nextui-org/react'
 import React, { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Providers({children}: {children: ReactNode}) {
   return (
