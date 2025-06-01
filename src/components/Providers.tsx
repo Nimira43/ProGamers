f09e2200b7ddf43a803fde23f7dc01c5e92e1812
Nimira43 <n.rabone72@aol.com>
2025-06-01 05:13:50 +0100

    Brought in ToastContainer into Providers.tsx.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,10 +2,12 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import React, { ReactNode } from 'react'
+import { ToastContainer } from 'react-toastify'
 
 export default function Providers({children}: {children: ReactNode}) {
   return (
     <NextUIProvider>
+      <ToastContainer />
       {children}
     </NextUIProvider>
   )
