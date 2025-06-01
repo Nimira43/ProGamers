0e2cdf177580890c496674e7194118c816d8eefe
Nimira43 <n.rabone72@aol.com>
2025-06-01 05:44:53 +0100

    Creating temporay styling to Home page - will be getting user session data onto this page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,15 @@ import Link from 'next/link'
 import React from 'react'
 import { MdOutlineGames } from "react-icons/md"
 
-export default function page() {
+export default function Home() {
   return (
-    <div>
-      <h1>Welcome to ProGamers</h1>
-
+    <div className='m-20'>
+      <h1 className='text-4xl mb-5'>Welcome to ProGamers</h1>
+      <h3 className='text-2xl text-main uppercase font-medium mb-5'>User Session Data</h3>
       <Button
         className='btn'
         as={Link}
         href='/members'
-        // color='primary'
-        // variant='bordered'
         startContent={<MdOutlineGames size={20} />}
       >
         Play
