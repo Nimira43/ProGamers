592049994222a042773d9939b81c41b24b86f8de
Nimira43 <n.rabone72@aol.com>
2025-06-01 05:38:58 +0100

    Created new custom btn class for styling buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@nextui-org/react'
+import Link from 'next/link'
 import React from 'react'
+import { MdOutlineGames } from "react-icons/md"
 
 export default function page() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome to ProGamers</h1>
+
+      <Button
+        className='btn'
+        as={Link}
+        href='/members'
+        // color='primary'
+        // variant='bordered'
+        startContent={<MdOutlineGames size={20} />}
+      >
+        Play
+      </Button>
     </div>
   )
 }
