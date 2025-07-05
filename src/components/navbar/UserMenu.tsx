5ebdc362b845ea6b5930dbdf81fd891da51618eb
Nimira43 <n.rabone72@aol.com>
2025-07-05 23:50:44 +0100

    Passing in user prop and starting to develop the dropdown menu

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,11 +1,14 @@
+import { Dropdown } from '@nextui-org/react'
 import { Session } from 'next-auth'
 
 type Props = {
   user: Session['user']
 }
 
-export default function UserMenu() {
+export default function UserMenu({user}: Props) {
   return (
-    <div>UserMenu</div>
+    <Dropdown>
+      
+    </Dropdown>
   )
 }
