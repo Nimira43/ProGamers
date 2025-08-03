7d3aa75ce3c82a2e8af5b47cc43c0a7672dab837
Nimira43 <n.rabone72@aol.com>
2025-08-03 12:20:31 +0100

    middleware - 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { auth } from './auth'
+import { publicRoutes } from './routes'
 
 export default auth((req) => {
   const {nextUrl} = req
   const isLoggedIn = !!req.auth
+
+  const isPublic = publicRoutes.includes(nextUrl.pathname)
+  
 })
