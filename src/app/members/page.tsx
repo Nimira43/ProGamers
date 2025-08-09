d7c0e1c7cc6857be9ee43c85b34976851359d02e
Nimira43 <n.rabone72@aol.com>
2025-08-09 23:44:07 +0100

    Importing getMembers

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import { getMembers } from '../actions/memberActions'
 
 export default async function MembersPage() {
+  const members = await getMembers()
+
   return (
     <div>
       <h3 className='text-3xl'>Members Page</h3>
