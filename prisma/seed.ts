bd79e91d9d9503bc83509d57ccd48e8fe18d6a0b
Nimira43 <n.rabone72@aol.com>
2025-08-09 22:32:25 +0100

    seedMembers - 2

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 import { membersData } from './membersData'
+import { hash } from 'bcryptjs'
 
 const prisma = new PrismaClient()
 
 async function seedMembers() {
   return membersData.map(async member => prisma.user.create({
-    
+    data: {
+      email: member.email,
+      emailVerified: new Date(),
+      name: member.name,
+      passwordHash: await hash('password', 10),
+    }
   }))
 }
