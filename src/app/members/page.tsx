5566c36f938be199c43eb2ad302571c72ac8c4e1
Nimira43 <n.rabone72@aol.com>
2025-08-16 11:09:33 +0100

    Passing in MemberCard

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { getMembers } from '../actions/memberActions'
+import MemberCard from './MemberCard'
 
 export default async function MembersPage() {
   const members = await getMembers()
 
   return (
-    <div className='mt-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8'>
-      
+    <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8'>    
         {members && members.map(member => (
-          <li key={member.id}>
-            {member.name}
-          </li>
+          <MemberCard 
+            member={member}
+            key={member.id}
+          />
         ))}
-
     </div>
   )
 }
