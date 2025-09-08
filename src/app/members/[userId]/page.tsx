f7e821171126cef5f085fbb22e901a990e9dcad4
Nimira43 <n.rabone72@aol.com>
2025-09-09 00:32:32 +0100

    Updated MemberDetailedPAge

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -4,9 +4,11 @@ import { notFound } from 'next/navigation'
 export default async function MemberDetailedPage({
   params
 }: {
-  params: {userId: string}
+  params: Promise<{userId: string}>
 }) {
-  const member = await getMemberByUserId(params.userId)
+  const {userId} = await params
+
+  const member = await getMemberByUserId(userId)
 
   if (!member) return notFound()
 
