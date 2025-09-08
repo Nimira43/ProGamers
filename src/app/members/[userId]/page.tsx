08239a2e487874f4b05ab6702af10901a30dc98e
Nimira43 <n.rabone72@aol.com>
2025-09-09 00:23:26 +0100

    Bringing in getMemberByUserId into MemberDetailedPage

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,8 +1,12 @@
-export default function MemberDetailedPage({
+import { getMemberByUserId } from '@/app/actions/memberActions'
+
+export default async function MemberDetailedPage({
   params
 }: {
   params: {userId: string}
 }) {
+  const member = await getMemberByUserId(params.userId)
+
   return (
     <div>{params.userId}</div>
   )
