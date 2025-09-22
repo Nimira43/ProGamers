942b887dbdd4b26c6e7ed288062c7eb6641bf900
Nimira43 <n.rabone72@aol.com>
2025-09-22 16:30:29 +0100

    Bringing in MemberSidebar

--- src/app/members/[userId]/layout.tsx
+++ src/app/members/[userId]/layout.tsx
@@ -1,5 +1,7 @@
 import { getMemberByUserId } from '@/app/actions/memberActions'
 import { ReactNode } from 'react'
+import MemberSidebar from '../MemberSidebar'
+import { notFound } from 'next/navigation'
 
 export default async function Layout({
   children, 
@@ -10,7 +12,16 @@ export default async function Layout({
 }) {
   const member = await getMemberByUserId(params.userId)
 
+  if (!member) return notFound()
+
   return (
-    <div>layout</div>
+    <div className='grid grid-cols-12 gap-5 h-[80vh]'>
+      <div className='col-span-3'>
+        <MemberSidebar 
+          member={member}
+        />
+      </div>
+
+    </div>
   )
 }
