dccf4d2d55c66d8f42dd976e18785419ca84dcea
Nimira43 <n.rabone72@aol.com>
2025-09-22 15:58:57 +0100

    Removing MemberSidebar from Teams page

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import MemberSidebar from '../members/MemberSidebar'
-// import Link from 'next/link'
+import Link from 'next/link'
 
 export default function ListsPage() {
   return (
     <div>
-      {/* <h3 className='text-3xl'>Teams Page</h3>
+      <h3 className='text-3xl'>Teams Page</h3>
       <Link href='/'>
         Home
-      </Link> */}
-      <MemberSidebar />
+      </Link>
     </div>
   )
 }
