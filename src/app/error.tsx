49fa2d664b244970b4f5fc7c19e72871b319eec9
Nimira43 <n.rabone72@aol.com>
2025-11-10 12:12:16 +0000

    Error card - 1

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,17 +1,20 @@
 'use client'
 
+import { Card } from "@nextui-org/react"
+
 export default function Error({
   error,
   reset
 }: {
-  error: Error & { 
-    digest?: string
-  }
+  error: Error & { digest?: string }
   reset: () => void
 }) {
 
   return (
-    <div>
+    <div className='flex items-center justify-center vertical-center'>
+      <Card className='w-2/5 mx-auto'>
+        
+      </Card>
       <h2>Something went wrong</h2>
       <button
         onClick={
