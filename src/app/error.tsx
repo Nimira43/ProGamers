c38bb09b2bf88513ff3bf8020a454036725cccde
Nimira43 <n.rabone72@aol.com>
2025-11-10 12:25:26 +0000

    Error card - 3

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Card, CardHeader } from '@nextui-org/react'
+import { Button, Card, CardBody, CardFooter, CardHeader } from '@nextui-org/react'
 import { ImConfused } from 'react-icons/im'
 
 export default function Error({
@@ -15,22 +15,29 @@ export default function Error({
     <div className='flex items-center justify-center vertical-center'>
       <Card className='w-2/5 mx-auto'>
         <CardHeader className='flex flex-col items-center justify-center'>
-          <div className='flex flex-col gap-2 itemce-center text-main'>
+          <div className='flex flex-col gap-2 items-center text-main'>
             <ImConfused size={30} />
-            <h1 className='text-3xl font-medium'>
-              Error!
+            <h1 className='text-3xl font-medium uppercase'>
+              Error
             </h1>
           </div>
         </CardHeader>
+        <CardBody>
+          <div className='flex justify-center text-dark'>
+            <h2>{error.message}</h2>
+          </div>
+        </CardBody>
+        <CardFooter className='flex justify-center'>
+          <Button 
+            className='bg-dark text-light uppercase'
+            onClick={
+              () => reset()
+            }
+          >
+            Try Again
+          </Button>
+        </CardFooter>
       </Card>
-      <h2>Something went wrong</h2>
-      <button
-        onClick={
-          () => reset()   
-        }
-      >
-        Try Again
-      </button>
     </div>
   )
 }
