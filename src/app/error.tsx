8e02ff0ec1c62003c33e34a7376bd62e70640b52
Nimira43 <n.rabone72@aol.com>
2025-11-10 12:07:22 +0000

    Error testing - 5

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,16 @@
+'use client'
+
 import { useEffect } from 'react'
 
-export default function error() {
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { 
+    digest?: string
+  }
+  reset: () => void
+}) {
   useEffect(() => {
     console.error(error)
   }, [error])
