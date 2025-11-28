392cb98c79e79f1ee4f0e115c37cb911d22a84d3
Nimira43 <n.rabone72@aol.com>
2025-11-28 20:32:08 +0000

    toggleLikeMember - 2

--- src/app/actions/likeActions.ts
+++ src/app/actions/likeActions.ts
@@ -1,8 +1,15 @@
 'use server'
 
+import { auth } from '@/auth'
+
 export async function toggleLikeMember(
   targetUserId: string,
   isLiked: boolean
 ) { 
-  
+  try {
+    const session = await auth()
+    const userId = session?.user?.id
+  } catch (error) {
+    
+  }
 }
