7430a8fbe286e290e4794e6d4c29372bcb02b505
Nimira43 <n.rabone72@aol.com>
2026-01-28 19:37:54 +0000

    useTransition and new LoadingComponent

---
+++ src/components/LoadingComponent.tsx
@@ -0,0 +1,18 @@
+import { Spinner } from '@nextui-org/react'
+
+export default function LoadingComponent({
+  label
+}: {
+  label?: string
+}) {
+  return (
+    <div className='fixed inset-0 flex justify-center items-center'>
+      <Spinner
+        label={label || 'Loading...'}
+        color='current'
+        labelColor='foreground'
+        
+      />
+    </div>
+  )
+}
