eff86f445425d157e5a6005a6e0343ed21d549ec
Nimira43 <n.rabone72@aol.com>
2026-03-29 12:47:43 +0100

    Setting main image.

--- src/app/actions/userAction.ts
+++ src/app/actions/userAction.ts
@@ -2,7 +2,7 @@
 
 import { memberEditSchema, MemberEditSchema } from '@/lib/schemas/memberEditSchema'
 import { ActionResult } from '@/types'
-import { Member } from '@prisma/client'
+import { Member, Photo } from '@prisma/client'
 import { getAuthUserId } from './authActions'
 import { prisma } from '@/lib/prisma'
 
@@ -62,6 +62,25 @@ export async function addimage(
           ]
         }
       }
+    })
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function setMainImage(photo: Photo) {
+  try {
+    const userId = await getAuthUserId()
+    
+    await prisma.user.update({
+      where: { id: userId },
+      data: { image: photo.url}
+    })
+
+    return prisma.member.update({
+      where: {userId},
+      data: { image: photo.url}
     })
   } catch (error) {
     console.log(error)
--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -1,14 +1,16 @@
 import { getAuthUserId } from '@/app/actions/authActions'
-import { getMemberPhotosByUserId } from '@/app/actions/memberActions'
+import { getMemberByUserId, getMemberPhotosByUserId } from '@/app/actions/memberActions'
 import ImageUploadButton from '@/components/imageUploadButton'
 import DeleteButton from '@/components/navbar/DeleteButton'
 import StarButton from '@/components/StarButton'
 import { CardBody, CardHeader, Divider, Image } from '@nextui-org/react'
 import MemberPhotoUpload from './MemberPhotoUpload'
 import MemberImage from '@/components/MemberImage'
+import MemberPhotos from '@/components/MemberPhotos'
 
 export default async function PhotosPage() {
   const userId = await getAuthUserId()
+  const member = await getMemberByUserId(userId)
   const photos = await getMemberPhotosByUserId(userId)
 
   return (
@@ -19,27 +21,11 @@ export default async function PhotosPage() {
       <Divider />
       <CardBody>
         <MemberPhotoUpload />
-        <div className='grid grid-cols-5 gap-3 p-5'>
-          {photos && photos?.map(photo => (
-            <div
-              key={photo.id}
-              className='relative'
-            >
-              <MemberImage photo={photo} />
-              <div className='absolute top-3 left-3 z-50'>
-                <StarButton
-                  selected={false}
-                  loading={false}
-                />
-              </div>
-              <div className='absolute top-3 right-3 z-50'>
-                <DeleteButton
-                  loading={false}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
+        <MemberPhotos 
+          photos={photos}
+          editing={true}
+          mainImageUrl={member?.image}
+        />
       </CardBody>
     </>
   )
